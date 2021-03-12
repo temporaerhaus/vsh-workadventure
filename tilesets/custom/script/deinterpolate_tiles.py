ba8367ee86a652252b4cc7b0846468b37ece96e0
jayAitch256 <qwertzqwertz543@gmail.com>
2021-03-12 16:38:33 +0100

    enhance deinterpolation script

--- tilesets/custom/script/deinterpolate_tiles.py
+++ tilesets/custom/script/deinterpolate_tiles.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Workaround script for WorkAdventure interpolation problems:
-Inserts an empty row below each row of tiles
+Workaround script for WorkAdventure interpolation problems
 
 Usage: python deinterpolate_tiles.py tileset.png tileset_new.png
 
@@ -20,16 +19,17 @@ from PIL import Image
 import sys
 import os
 
+TILE_SIZE = 32
 BASE_DIR = os.getcwd()
 SOURCE_FILENAME = sys.argv[1]
 RESULT_FILENAME = sys.argv[2]
 
 IMG = Image.open(SOURCE_FILENAME)
-TILESHEET_WIDTH, TILESHEET_HEIGTH = IMG.size
+TILESHEET_WIDTH, TILESHEET_HEIGHT = IMG.size
 
 
 class Row():
-    def __init__(self, width=TILESHEET_WIDTH, height=32):
+    def __init__(self, width=TILESHEET_WIDTH * 2 + 64, height=TILE_SIZE):
         self.width = width
         self.height = height
         self.remainingWidth = width
@@ -38,10 +38,10 @@ class Row():
     def append(self, Img):
         x, y = Img.size
         if (self.remainingWidth - x) < 0:
-            raise Exception('ImageTooLarge')
+            pass #raise Exception('ImageTooLarge')
         self.height = max(self.height, y)
-        if self.height > TILESHEET_HEIGTH:
-            raise Exception("Tilesheet full")
+        if self.height > TILESHEET_HEIGHT:
+            pass #raise Exception("Tilesheet full")
         self.remainingWidth = self.remainingWidth - x
         self.sprites.append(Img.convert(mode='RGBA'))
         return self.remainingWidth
@@ -57,7 +57,7 @@ class Row():
 
 
 class Spritesheet():
-    def __init__(self, width=TILESHEET_WIDTH, height=TILESHEET_HEIGTH):
+    def __init__(self, width=TILESHEET_WIDTH * 2 + 64, height=TILESHEET_HEIGHT * 2  + 64):
         self.width = width
         self.height = height
         self.rows = []
@@ -79,12 +79,38 @@ class Spritesheet():
 
 sheet = Spritesheet()
 
-for i in range(0, int(TILESHEET_HEIGTH / 32) - 1):
-    row = Row()
-    tiles = IMG.crop(
-        (0, i * 32, TILESHEET_WIDTH, (i + 1) * 32))
-    row.append(tiles)
-    sheet.append(row.render())
-    sheet.append(Row().render())
+sheet.append(Row(height=1).render())
+
+for i in range(0, TILESHEET_HEIGHT - 32, 32):
+
+    tiles = Row()
+    top_border = Row(height=1)
+    bottom_border = Row(height=1)
+
+    tiles.append(Image.new('RGBA', (1, 32), (0, 0, 0, 0)))
+
+    for j in range(0, TILESHEET_WIDTH - 32, 32):
+        top_border.append(Image.new('RGBA', (32, 1), (0, 0, 0, 0)))
+        bottom_border.append(Image.new('RGBA', (32, 1), (0, 0, 0, 0)))
+
+        top_border.append(IMG.crop((j, i, j + 32, i + 1)))
+        bottom_border.append(IMG.crop((j, i + 31, j + 32, i + 32)))
+
+        tiles.append(Image.new('RGBA', (30, 32), (0, 0, 0, 0)))
+        tiles.append(IMG.crop((j, i, j + 1, i + 32)))
+        tiles.append(IMG.crop((j, i, j + 32, i + 32)))
+        tiles.append(IMG.crop((j + 31, i, j + 32, i + 32)))
+
+    top_border.append(Image.new('RGBA', (32, 1), (0, 0, 0, 0)))
+    bottom_border.append(Image.new('RGBA', (32, 1), (0, 0, 0, 0)))
+    tiles.append(Image.new('RGBA', (31, 32), (0, 0, 0, 0)))
+
+
+    sheet.append(Row(height=30).render())
+    sheet.append(top_border.render())
+    sheet.append(tiles.render())
+    sheet.append(bottom_border.render())
+
+sheet.append(Row(height=31).render())
 
 sheet.render().save(RESULT_FILENAME)
